d67e2469d35766b5c7e382739e191a537522dd85
Mohammed Ameen <m.ameen2108@gmail.com>
2024-10-13 12:17:57 +0530

    Tests Added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,29 +1,76 @@
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
-
 describe("TodoList Test Suite", () => {
+  let list; // Declare a variable to hold the todoList instance
+
   beforeAll(() => {
-    add({
+    list = todoList(); // Initialize the instance
+    const today = new Date().toISOString().slice(0, 10);
+    list.add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: today,
     });
   });
 
   test("Should Add new Todo", () => {
-    const todoItemsCount = all.length;
-    add({
+    const todoItemsCount = list.all.length;
+    list.add({
       title: "Test todo",
       completed: false,
       dueDate: new Date().toISOString().slice(0, 10),
     });
-    expect(all.length).toBe(todoItemsCount + 1);
+    expect(list.all.length).toBe(todoItemsCount + 1);
   });
 
   test("Should mark a todo as Complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+    expect(list.all[0].completed).toBe(false);
+    list.markAsComplete(0);
+    expect(list.all[0].completed).toBe(true);
+  });
+
+  test("Should return Overdue items", () => {
+    expect(list.overdue().length).toBe(0);
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const formattedYesterday = yesterday.toISOString().slice(0, 10);
+
+    list.add({
+      title: "Test todo1",
+      completed: false,
+      dueDate: formattedYesterday,
+    });
+
+    expect(list.overdue().length).toBe(1);
+  });
+
+  test("Should return Due Today items", () => {
+    expect(list.dueToday().length).toBe(2);
+    const today = new Date().toISOString().slice(0, 10);
+
+    list.add({
+      title: "Test todo2",
+      completed: false,
+      dueDate: today,
+    });
+
+    expect(list.dueToday().length).toBe(3);
+  });
+
+  test("Should return Due Later items", () => {
+    expect(list.dueLater().length).toBe(0);
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const formattedTomorrow = tomorrow.toISOString().slice(0, 10);
+
+    list.add({
+      title: "Test todo3",
+      completed: false,
+      dueDate: formattedTomorrow,
+    });
+
+    expect(list.dueLater().length).toBe(1);
   });
 });
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,5 +1,6 @@
 const todoList = () => {
   all = [];
+  const today = new Date().toISOString().slice(0, 10);
   const add = (todoItem) => {
     all.push(todoItem);
   };
