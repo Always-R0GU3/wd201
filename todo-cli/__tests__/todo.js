6d232505a9732d8b075cf3665af896bd089b4792
Mohammed Ameen <m.ameen2108@gmail.com>
2024-10-13 11:15:28 +0530

    Add

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,31 +1,29 @@
+const todoList = require("../todo");
 
-const todoList=require("../todo")
+const { all, markAsComplete, add } = todoList();
 
-const {all,markAsComplete,add}=todoList()
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+  });
 
-describe("TodoList Test Suite",() => {
+  test("Should Add new Todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
 
-    beforeAll(() => {
-        add({
-            title: "Test todo",
-            completed: false,
-            dueDate: new Date().toISOString().slice(0, 10)
-        })
-    })
-
-    test("Should Add new Todo",() => {
-            const todoItemsCount=all.length
-            add({
-                title:"Test todo",
-                completed:false,
-                dueDate: new Date().toISOString().slice(0,10)
-            })
-            expect(all.length).toBe(todoItemsCount+1)
-    })
-
-    test("Should mark a todo as Complete",() => {
-        expect(all[0].completed).toBe(false)
-        markAsComplete(0)
-        expect(all[0].completed).toBe(true)
-    })
-})
+  test("Should mark a todo as Complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,78 +1,83 @@
 const todoList = () => {
-    all = []
-    const add = (todoItem) => {
-        all.push(todoItem)
-    }
-    const markAsComplete = (index) => {
-        all[index].completed = true
-    }
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-    const overdue = () => {
-        // Write the date check condition here and return the array
-        // of overdue items accordingly.
-        let overdueTodo = []
-        all.forEach(({title,dueDate,completed}) => {
-            if (dueDate < today){
-                overdueTodo.push({title,dueDate,completed})
-            }
-        })
-        return overdueTodo
-    }
+  const overdue = () => {
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
+    let overdueTodo = [];
+    all.forEach(({ title, dueDate, completed }) => {
+      if (dueDate < today) {
+        overdueTodo.push({ title, dueDate, completed });
+      }
+    });
+    return overdueTodo;
+  };
 
-    const dueToday = () => {
-        // Write the date check condition here and return the array
-        // of todo items that are due today accordingly.
-        let dueodayTodo = []
-        all.forEach(({ title, dueDate, completed }) => {
-            if (dueDate === today) {
-                dueodayTodo.push({ title, dueDate, completed,fl:1 })
-            }
-        })
-        return dueodayTodo
-    }
+  const dueToday = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+    let dueodayTodo = [];
+    all.forEach(({ title, dueDate, completed }) => {
+      if (dueDate === today) {
+        dueodayTodo.push({ title, dueDate, completed, fl: 1 });
+      }
+    });
+    return dueodayTodo;
+  };
 
-    const dueLater = () => {
-        // Write the date check condition here and return the array
-        // of todo items that are due later accordingly.
-        let duelaterTodo = []
-        all.forEach(({ title, dueDate, completed }) => {
-            if (dueDate > today) {
-                duelaterTodo.push({ title, dueDate, completed })
-            }
-        })
-        return duelaterTodo
-    }
+  const dueLater = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
+    let duelaterTodo = [];
+    all.forEach(({ title, dueDate, completed }) => {
+      if (dueDate > today) {
+        duelaterTodo.push({ title, dueDate, completed });
+      }
+    });
+    return duelaterTodo;
+  };
 
-    const toDisplayableList = (list) => {
-        // Format the To-Do list here, and return the output string
-        // as per the format given above.
-        let finalString=[]
-        list.forEach(({title,dueDate,completed,fl}) => {
-            if(fl) {
-                if (completed) { finalString.push(`[x] ${title}`) }
-                else { finalString.push(`[ ] ${title}`) }
-            }
-            else{
-                if (completed) { finalString.push(`[x] ${title} ${dueDate}`) }
-                else { finalString.push(`[ ] ${title} ${dueDate}`) }
-            }
-        })
-        return finalString.join("\n")
-    }
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+    let finalString = [];
+    list.forEach(({ title, dueDate, completed, fl }) => {
+      if (fl) {
+        if (completed) {
+          finalString.push(`[x] ${title}`);
+        } else {
+          finalString.push(`[ ] ${title}`);
+        }
+      } else {
+        if (completed) {
+          finalString.push(`[x] ${title} ${dueDate}`);
+        } else {
+          finalString.push(`[ ] ${title} ${dueDate}`);
+        }
+      }
+    });
+    return finalString.join("\n");
+  };
 
-    return {
-        all,
-        add,
-        markAsComplete,
-        overdue,
-        dueToday,
-        dueLater,
-        toDisplayableList
-    };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
 };
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
 
-module.exports=todoList
+module.exports = todoList;
